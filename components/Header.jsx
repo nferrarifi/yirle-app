57d4021e5a8c8a9d7c1a4d8eaa3c77b7878c7e9c
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2023-10-10 13:10:55 -0300

    styles for header

--- components/Header.jsx
+++ components/Header.jsx
@@ -29,7 +29,7 @@ const Header = ({user}) => {
     <header className={`w-full flex justify-center border-b border-b-foreground/10 h-16 z-20 sticky top-0 ${isScrolled ? " bg-[#f2f2f2]" : ""} transition-all duration-300`}>
     <div className={`w-full max-w-8xl lg:px-8 ${isScrolled ? "lg:px-4" : "pt-8 " } flex justify-between items-center text-sm transition-all`}>
       <div className='flex items-center space-x-12'>
-        <div className={`text-6xl cursor-default ${isScrolled ? "text-2xl" : ""} transition-all text-[#3498db] font-semibold`}><a href='/'><span className='text-[]'>Y</span>irle</a></div>
+        <div className={`text-6xl cursor-default ${isScrolled ? "text-xl" : ""} transition-all text-[#3498db] font-semibold`}><a href='/'><span className='text-[]'>Y</span>irle</a></div>
         <ul className='flex space-x-4 text-gray-500 '>
           <li className={user ? "hover:text-gray-950 transition-all" : "hidden"}><Link href="/records">My Records</Link></li>
           <li className={user ? "hover:text-gray-950 transition-all" : "hidden"}><Link href="/records/add">Add Records</Link></li>
